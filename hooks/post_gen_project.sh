e16e1f38e8e2de3eaa484a7a9375f0b3e725df6a
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2024-03-11 14:45:44 +0300

    update pdm

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cat linters_config.toml >> pyproject.toml
 rm linters_config.toml
+mv src_/* src/
+rm -rf src_/
+pre-commit install --install-hooks
--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 git init
-pre-commit install
-pdm init
+pdm init --backend pdm-backend
 pdm install
-pdm add mypy ruff -G dev
+pdm add mypy ruff -dG lint
